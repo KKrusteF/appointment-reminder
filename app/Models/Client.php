b5025c8338eaae7166638f4ae3a62ff1463193e5
Kristiyan <kristiyan@perspectiveunity.com>
2025-07-02 21:22:13 +0300

    add Clients

---
+++ app/Models/Client.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ClientFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    /** @use HasFactory<ClientFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'timezone',
+    ];
+
+    protected $attributes = [
+        'timezone' => 'UTC',
+    ];
+}
