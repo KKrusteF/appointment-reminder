8184c66aa31504e873bbaa7a6918f62abfaee219
Kristiyan <kristiyan@perspectiveunity.com>
2025-07-04 14:14:12 +0300

    add route binding for appointment model based on authenticated user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Appointment;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::bind('appointment', function ($value) {
+            return Appointment::where('user_id', auth()->id())->findOrFail($value);
+        });
     }
 }
